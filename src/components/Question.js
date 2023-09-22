2dcc6199e288e968661ab3da2d8c1fbaeda9cd86
Samuel <samuelfras@gmail.com>
2023-09-22 14:47:08 -0600

    Lab Complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+useEffect(() => {
+  let timer;
+
+  const startTimer = () => {
+    timer = setTimeout (() => {
+      if (timeRemaining > 1){
+        setTimeRemaining((prevTime) => prevTime - 1);
+        startTimer();
+      
+      }else {
+        onAnswered(false)
+        setTimeRemaining (10)
+      }
+    }, 1000)
+  };
+  startTimer();
+
+  return () => {
+    clearTimeout(timer)
+  };
+}, [timeRemaining, onAnswered])
+
+
+
+//   useEffect(() => {
+//     const timerInterval = setInterval(() => {
+//       if (timeRemaining > 0) {
+//         setTimeRemaining((prevTime) => prevTime - 1)
+//       } else {
+//         clearInterval(timerInterval);
+//         setTimeRemaining(10)
+//         onAnswered(false)
+//       }
+//     } , 1000);
+//     return () => {
+//       clearInterval(timerInterval);
+//     };
+// }, [timeRemaining, onAnswered]);
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+ 
 
   const { id, prompt, answers, correctIndex } = question;
 
